0a067522ab263c263b5f4e876350d0f91945b811
kumpelblase2 <tim@infinityblade.de>
2015-01-08 10:49:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function Plugin(app, chat, options)
+{
+    if(!options) {
+        throw new Error('No options specified for plugin.');
+    }
+
+    if(!options.name) {
+        throw new Error('No name specified for plugin');
+    }
+
+    this.name = options.name;
+    if(!options.version) {
+        throw new Error('No version specified for plugin ' + this.name);
+    }
+
+    this.version = options.version;
+    if(options.models) {
+        this.models = options.models;
+    }
+}
+
+Plugin.prototype.init = function(callback) {
+    callback();
+};
+
+Plugin.prototype.enable = function(callback) {
+    callback();
+};
+
+Plugin.prototype.disable = function() {
+};
+
+module.exports = Plugin;
